71e238e62cab505b83b2f43ea943f598e04e1532
Martin Schmitt <you@example.com>
2020-06-08 13:21:27 -0400

    elm counter

--- src/Main.elm
+++ src/Main.elm
@@ -1,81 +1,47 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, code, div, pre, text)
+import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
-import Random exposing ( Generator)
+
 
 type alias Model =
-      Expr
+    { count : Int }
 
 
-type Expr
-    = Integer Int
-    | Add Expr Expr
-    | Multiply Expr Expr
-
+initialModel : Model
+initialModel =
+    { count = 0 }
 
 
 type Msg
-    = NewExpr Expr
-    | GetNewExpr
+    = Increment
+    | Decrement
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            { model | count = model.count + 1 }
+
+        Decrement ->
+            { model | count = model.count - 1 }
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [button [onClick Increment ] [ text "+1" ]
+        , div[] [ text <| String.fromInt model.count ]
+        , button [ onClick Decrement ] [ text "-1"]
+        ]
 
 
 main : Program () Model Msg
 main =
-    Browser.element
-      { init = always ( Integer 0, Cmd.none )
-      , view = view
-      , update = update
-      , subscriptions = always Sub.none
-      }
-
-
-view : Expr -> Html Msg
-view expr =
-    div []
-        [button [onClick GetNewExpr ][ text "New Expression" ]
-        , code []
-            [ pre []  [ text <| Debug.toString expr ]
-            ]
-        ]
-
-
-update : Msg -> Model -> (Model, Cmd Msg )
-update msg model =
-    case msg of
-        GetNewExpr ->
-            ( model, Random.generate NewExpr expression )
-
-        NewExpr expr ->
-            ( expr, Cmd.none )
-
-
-
-
-
-
-expression : Generator Expr
-expression =
-    Random.weighted (50, integer ) [  (25, addition ), (25, multiplication ) ]
-        |> Random.andThen identity
-
-
-integer : Generator Expr
-integer =
-    Random.int 1 100
-        |> Random.map Integer
-
-
-addition : Generator Expr
-addition =
-    Random.map2 Add
-        (Random.lazy (\_ -> expression))
-        (Random.lazy (\_ -> expression))
-
-
-multiplication : Generator Expr
-multiplication =
-      Random.map2 Multiply
-          (Random.lazy ( \_ -> expression ))
-          (Random.lazy (\_ -> expression ))
+    Browser.sandbox
+        { init =initialModel
+        , view = view
+        , update = update
+        }
